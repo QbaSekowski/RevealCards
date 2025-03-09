742db27775b778552b447b4f6e6d64affcb27ed5
Jakub Sekowski <sekowskijakub@gmail.com>
2025-03-09 10:31:48 +0100

    Implementation added.

--- src/main/java/mate/academy/RevealCards.java
+++ src/main/java/mate/academy/RevealCards.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RevealCards {
     public static int[] deckRevealedIncreasing(int[] deck) {
-
+        Arrays.sort(deck);
+        int n = deck.length;
+        int[] result = new int[n];
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            queue.offer(i);
+        }
+        for (int card : deck) {
+            result[queue.poll()] = card;
+            if (!queue.isEmpty()) {
+                queue.offer(queue.poll());
+            }
+        }
+        return result;
     }
 }
